4466d494c134f41338e7454468dabd762d601d06
JAMBITI <biti.jamal@gmail.com>
2023-08-08 05:35:23 -0700

    lowercs

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for a in range(10):
     for b in range(10):
-        if a > b:
+        if b > i:
             if a * 10 + b != 89:
                 print("{:02d}".format(a * 10 + b), end=', ')
             else:
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# This is a function that checks for lowercase character.
+
+def islower(c):
+    return 'a' <= c <= 'z'
