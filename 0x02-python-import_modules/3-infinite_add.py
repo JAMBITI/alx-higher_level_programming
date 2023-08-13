289db45cc78a02aad777862fedb311c32ceb2f6a
JAMBITI <biti.jamal@gmail.com>
2023-08-13 07:51:50 -0700

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-from add_0 import add
+    import sys
 if __name__ == "__main__":
-        a = 1
-        b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+
+    a = 0
+    args = sys.argv
+
+    if len(args) > 1:
+        for arg in sys.argv[1:]:
+        a += int(arg)
+
+    print(a)
