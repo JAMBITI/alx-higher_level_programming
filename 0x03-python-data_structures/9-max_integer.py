098980fa79c2470ede3fdf5e8333a360f1df0783
JAMBITI <biti.jamal@gmail.com>
2023-08-14 11:07:37 -0700

    ok

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if my_list == []:
         return None
-    return max(my_list)
+    my_list.sort()
+    return my_list[-1]
