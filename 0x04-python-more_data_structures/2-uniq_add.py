687a39b63059cab163c1b41dbe876e3d57a9b0c5
JAMBITI <biti.jamal@gmail.com>
2023-08-16 06:38:04 -0700

    task 8

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
-    unique_values = {}
-    summ = 0
-    
-    for num in my_list:
-        if num not in unique_values:
-            unique_values[num] = True
-            summ += num
-            
-    return summ
+unique_values = {}
+summ = 0
+
+for num in my_list:
+if num not in unique_values:
+unique_values[num] = True
+summ += num
+
+return summ
---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    updated_dict = {**a_dictionary, key: value}
+    return updated_dict
