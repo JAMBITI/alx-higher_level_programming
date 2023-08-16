51c995e42b86686fad0e9da15b08df0f40e00d52
JAMBITI <biti.jamal@gmail.com>
2023-08-16 07:10:54 -0700

    update

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+
 def simple_delete(a_dictionary, key=""):
-    return {k: v for k, v in a_dictionary.items() if k != key}
+    a_dictionary.pop(key, None)
+    return a_dictionary
