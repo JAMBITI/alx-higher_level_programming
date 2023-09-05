178cd8d4e77843e360000515984fe4173f14b1ae
JAMBITI <biti.jamal@gmail.com>
2023-09-05 04:49:20 -0700

    ever is obj

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 
-def magic_string(counter=[0]):
-    counter[0] += 1
-    return "BestSchool" + (", BestSchool" * (counter[0] - 1))
+def magic_string():
+    i = 0
+    while True:
+        i += 1
+        yield "BestSchool" + (", BestSchool" * (i - 1))
 
-# Example usage
-for _ in range(5):
-    print(magic_string())
+# Example usage:
+gen = magic_string()
+for _ in range(5):  # Generate 5 strings
+    print(next(gen))
