2a70b1a52725a61ba2a1fc1d6b39e4fbb3d782fa
JAMBITI <biti.jamal@gmail.com>
2023-09-06 05:34:41 -0700

    advc

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module for class LockedClass
+"""
+
+
+class LockedClass:
+    """
+    class LockedClass
+    creating of specific
+    class or object attributes
+    """
+    __slots__ = ["first_name"]
