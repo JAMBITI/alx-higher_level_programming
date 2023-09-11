1bafb9dd468f69f82f00764aa0a840d0b9025211
JAMBITI <biti.jamal@gmail.com>
2023-09-11 06:25:44 -0700

    inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,37 +1,23 @@
 #!/usr/bin/python3
-"""Square Module - Defines a Square class"""
+"""
+Module for the class Square
+"""
 
-from typing import Any
-Rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """Defines a Square class."""
+    """
+    Class Square that inherits from Rectangle
+    """
 
-    def __init__(self, size: int):
+    def __init__(self, size):
         """
-        Initializes a new Square instance.
+        Instantiation with size
 
         Args:
-            size (int): The size of the square's sides.
+            size (int)
         """
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         self.__size = size
-
-    def area(self) -> int:
-        """
-        Computes the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size * self.__size
-
-    def __str__(self) -> str:
-        """
-        Generates a string representation of the square.
-
-        Returns:
-            str: A string describing the square.
-        """
-        return f"[Square] {self.__size}/{self.__size}"
+        super().__init__(size, size)
