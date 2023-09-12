add5813dc7907f80940702093754ff2c34d291f7
JAMBITI <biti.jamal@gmail.com>
2023-09-12 05:04:38 -0700

    inp-outp

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,9 +3,9 @@
 
 
 class Student:
-"""Represents a student."""
+    """Represent a student."""
 
-def __init__(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
 """Initialize a new Student object.
 
 Args:
@@ -13,10 +13,10 @@ first_name (str): The first name of the student.
 last_name (str): The last name of the student.
 age (int): The age of the student.
 """
-self.first_name = first_name
-self.last_name = last_name
-self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-def to_json(self):
-"""Return a dictionary representation of the Student object."""
-return self.__dict__
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
